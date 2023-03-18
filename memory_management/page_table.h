1bbab9414185aa4a19d8ea1ba2fdd227e8d6e1fe
Ty Bui <67993925+thaibui2308@users.noreply.github.com>
2023-03-18 16:47:34 -0700

    additional methods for PageTable and Level struct

--- memory_management/page_table.h
+++ memory_management/page_table.h
@@ -7,13 +7,18 @@
 #include "map.h"
 
 typedef struct Level {
+    void* pageTablePtr;
     int depth;
     bool isLeaf;
     Level** next;
-
+    mapPtr map;
 } Level;
 
 typedef struct Level * levelPtr;
+
+/* Methods for Level struct */
+levelPtr createLevel(int entrycount);
+void table_insert_vpn2pfn(levelPtr levelPtr, unsigned int address, unsigned int frame);
 
 typedef struct PageTable {
     levelPtr tableRootNode;
@@ -24,5 +29,11 @@ typedef struct PageTable {
 } PageTable;
 
 typedef struct PageTable * tablePtr;
+
+/* Methods for PageTable struct */
+tablePtr createPageTable(int levelCount);
+unsigned int getBitMaskForLevel(int level);
+unsigned int getBitShiftForLevel(int level);
+void ptab_insert_vpn2pfn(tablePtr pageTablePtr, unsigned int address, unsigned int frame);
 
 #endif
